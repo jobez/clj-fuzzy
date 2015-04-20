0b0b028b558d6932bdf91602abb3b81725cf5b9f
Johann Bestowrous <johann.bestowrous@gmail.com>
2015-04-20 15:00:39 -0400

    remove cross over vestiges, bump up cljs version

--- project.clj
+++ project.clj
@@ -3,33 +3,13 @@
   :url "http://yomguithereal.github.io/clj-fuzzy/"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2665"]]
+  :dependencies [[org.clojure/clojure "1.7.0-beta1"]
+                 [org.clojure/clojurescript "0.0-3196"]]
   :plugins [[lein-kibit "0.0.8"]
-            [lein-cljsbuild "1.0.3"]]
+            [lein-cljsbuild "1.0.5"]]
   :cljsbuild
-  {:crossovers [clj-fuzzy.caverphone
-                clj-fuzzy.cologne
-                clj-fuzzy.dice
-                clj-fuzzy.double-metaphone
-                clj-fuzzy.hamming
-                clj-fuzzy.helpers
-                clj-fuzzy.jaccard
-                clj-fuzzy.jaro_winkler
-                clj-fuzzy.lancaster
-                clj-fuzzy.levenshtein
-                clj-fuzzy.lovins
-                clj-fuzzy.match_rating
-                clj-fuzzy.metaphone
-                clj-fuzzy.nysiis
-                clj-fuzzy.porter
-                clj-fuzzy.schinke
-                clj-fuzzy.soundex
-                clj-fuzzy.tversky],
-   :builds
-   [{:crossover-jar true,
-     :source-paths ["src-cljs"],
-     :crossover-path "src-cljs/clj_fuzzy",
+  {:builds
+   [{:source-paths ["src"],
      :compiler
      {:pretty-print false,
       :output-to "src-js/clj-fuzzy.temp.js",
